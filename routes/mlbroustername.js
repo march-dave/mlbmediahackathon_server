8573026ad78c3edab8bdded8147229d6b315daea
march-dave <ysangy@gmail.com>
2017-02-11 12:02:51 -0500

    user router

--- routes/mlbroustername.js
+++ routes/mlbroustername.js
@@ -24,32 +24,16 @@ var Client = require('../models/mlb');
 
 router.route('/')
     .get((req, res) => {
-        // Client.find({}, (err, trees) => {
-        //     if (err) {
-        //         res.status(400).send(err);
-        //     } else {
-        //         res.send('trees');
-        //     }
-        // });
-
-        var options = { method: 'GET',
-        url: 'http://mlb.mlb.com/lookup/named.roster_all.bam',
-        qs: { team_id: '\'141\'' },
-        headers:
-        { 'postman-token': 'e24805c9-2d42-32b5-0ac3-dce901cf743a',
-            'cache-control': 'no-cache',
-            'content-type': 'application/x-www-form-urlencoded' },
-        form: { imageurl: 't', like: 'g', dislike: 's' } };
-
-        request(options, function (error, response, body) {
-        // if (error) throw new Error(error);
-
-        // console.log(body);
-
-        res.send(body);
+        
+        Client.find({}, (err, trees) => {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.send('trees');
+            }
         });
 
-
+      
     })
 
 router.route('/:id')

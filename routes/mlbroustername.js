c1b1fc06e3c4752363211bc671854bbe82d6da91
march-dave <ysangy@gmail.com>
2017-02-11 11:27:21 -0500

    mlb public aapi call

--- routes/mlbroustername.js
+++ routes/mlbroustername.js
@@ -19,17 +19,37 @@
 var express = require('express');
 var router = express.Router();
 
+var request = require("request");
 var Client = require('../models/mlb');
 
 router.route('/')
     .get((req, res) => {
-        Client.find({}, (err, trees) => {
-            if (err) {
-                res.status(400).send(err);
-            } else {
-                res.send('trees');
-            }
+        // Client.find({}, (err, trees) => {
+        //     if (err) {
+        //         res.status(400).send(err);
+        //     } else {
+        //         res.send('trees');
+        //     }
+        // });
+
+        var options = { method: 'GET',
+        url: 'http://mlb.mlb.com/lookup/named.roster_all.bam',
+        qs: { team_id: '\'141\'' },
+        headers:
+        { 'postman-token': 'e24805c9-2d42-32b5-0ac3-dce901cf743a',
+            'cache-control': 'no-cache',
+            'content-type': 'application/x-www-form-urlencoded' },
+        form: { imageurl: 't', like: 'g', dislike: 's' } };
+
+        request(options, function (error, response, body) {
+        // if (error) throw new Error(error);
+
+        // console.log(body);
+
+        res.send(body);
         });
+
+
     })
 
 router.route('/:id')
